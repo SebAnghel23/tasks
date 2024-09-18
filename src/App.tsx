00f2b8401bfeb852605eaa640160aa72d601066c
Sebastian Anghel <sebastian.anghel@maine.edu>
2024-09-17 21:06:04 -0400

    Add Name to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <p> by Sebastian Anghel</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
