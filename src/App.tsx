ffc57510aa2999fae91069d174baafa72c8f3f8b
Sebastian Anghel <sebastian.anghel@maine.edu>
2024-09-19 19:05:00 -0400

    Updated markup and CSS to fix unit tests.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,43 @@ import React from "react";
 import "./App.css";
 
 function App(): React.JSX.Element {
+
+     const handleClick = () => {
+        console.log("Hello World!");
+    };
+
     return (
         <div className="App">
-            <header className="App-header">
+            <div className="App-banner" role="banner">
+                Stock Market Ticker Goes Here
+            </div>
+            {/* Header section */}
+            <header className="App-header" role="heading">
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p> by Sebastian Anghel</p>
+
+            {/* image loaded in */}
+            <img src="../assets/images/pet-ada.jpg" alt="A picture of my dog Ada" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            {/* List with 3 elements */}
+            <ul role="list">
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+            </ul>
+
+            {/* Bootstrap Button */}
+            <button className="btn btn-primary" onClick={handleClick}>
+                Log Hello World
+            </button>
+
+            {/* Background Color to red */}
+            <div style={{ backgroundColor: "red", height: "50px", width: "50px" }}> red box </div>
         </div>
     );
 }
